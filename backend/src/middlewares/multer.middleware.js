ea947e5b6cb79876c01111c81c960553674be2f9
Manik <manikkori697@gmail.com>
2026-09-15 21:22:03 +0530

    Added middleware for storage configuration

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+//storage configuration
+const storage = multer.diskStorage({
+
+    destination: function (req, file, cb){
+        cb(null, "./tmp")
+    },
+
+    filename: function (req, file, cb){
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
